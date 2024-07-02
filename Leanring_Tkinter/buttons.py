d2c185ae0b516d2d0e2d2cb762664d12d83c3a1d
Henok100 <henokgashaw15@gmail.com>
2024-07-02 14:08:38 +0300

    Final

--- Leanring_Tkinter/buttons.py
+++ Leanring_Tkinter/buttons.py
@@ -4,7 +4,12 @@ from tkinter import *
 # Create an instance of the Tk class, which initializes the Tkinter application
 root = Tk()
 
-
+def myClick():
+    myLabel = Label(root, text="Look! I clicked a button!!")
+    myLabel.pack()
+     
+myButton = Button(root, text='Click Me!', command=myClick, fg='blue', bg='yellow')
+myButton.pack()
 
 # Enter the Tkinter event loop to keep the window open and responsive
-root.mainloop()
+root.mainloop() 
