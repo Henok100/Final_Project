abb2dfe03ad3dc93a76afab45220860f28aa55cc
Henok100 <henokgashaw15@gmail.com>
2024-07-02 14:46:05 +0300

    Final

--- Leanring_Tkinter/entry.py
+++ Leanring_Tkinter/entry.py
@@ -4,12 +4,16 @@ from tkinter import *
 # Create an instance of the Tk class, which initializes the Tkinter application
 root = Tk()
 
+e = Entry(root, width=50)
+e.pack()
+e.insert(0, "Enter your name: ")
+
 def myClick():
-    myLabel = Label(root, text="Look! I clicked a button!!")
+    hello = "Hello " + e.get()
+    myLabel = Label(root, text=hello)
     myLabel.pack()
      
-myButton = Button(root, text='Click Me!', command=myClick, fg='blue', bg='yellow')
+myButton = Button(root, text='Enter Your Name', command=myClick, fg='blue', bg='yellow')
 myButton.pack()
 
-# Enter the Tkinter event loop to keep the window open and responsive
 root.mainloop() 
