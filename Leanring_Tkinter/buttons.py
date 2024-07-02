f139f456da21f752235711a7e9f51b7bb2594ccf
Henok100 <henokgashaw15@gmail.com>
2024-07-02 13:54:57 +0300

    F

---
+++ Leanring_Tkinter/buttons.py
@@ -0,0 +1,10 @@
+# Import everything from the tkinter module
+from tkinter import *
+
+# Create an instance of the Tk class, which initializes the Tkinter application
+root = Tk()
+
+
+
+# Enter the Tkinter event loop to keep the window open and responsive
+root.mainloop()
--- Leanring_Tkinter/grid.py
+++ Leanring_Tkinter/grid.py
@@ -5,10 +5,12 @@ from tkinter import *
 root = Tk()
 
 # Create a Label widget with the parent 'root' and the text 'Hello Henok!'
-myLabel = Label(root, text='Hello Henok!')
+myLabel1 = Label(root, text='Hello Henok!').grid(row=0, column=0)
+myLabel2 = Label(root, text='My name is Henok Gashaw').grid(row=1, column=5)
 
 # Use the pack method to add the Label widget to the window
-myLabel.pack()
+# myLabel1.grid(row=0, column=0)
+# myLabel2.grid(row=1, column=5)
 
 # Enter the Tkinter event loop to keep the window open and responsive
 root.mainloop()
